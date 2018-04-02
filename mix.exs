4452f8c586b05cb90ed3da8b5821c6c5ff1f0925
Dung Nguyen <flex@Dungs-MacBook-Pro.local>
2018-04-02 21:19:23 +0700

    update package info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,19 @@ defmodule Yexdl.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: "A simple tool to extract mp4 link from youtube video"
+    ]
+  end
+
+  def package do
+    [
+      name: :google_books,
+      files: ["lib", "mix.exs"],
+      maintainers: ["Dung Nguyen"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/bluzky/yexdl"}
     ]
   end
 
@@ -21,9 +33,9 @@ defmodule Yexdl.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:hackney, "~> 1.11"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:hackney, "~> 1.11"},
+      {:earmark, ">= 0.0.0", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
